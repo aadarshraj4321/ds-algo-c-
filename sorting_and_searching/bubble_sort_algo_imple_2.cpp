f7c3e70c664142d786e25a28cc28e8d28e5055b1
aadarshraj4321 <aadarshraj4321@gmail.com>
2021-08-30 11:29:33 -0400

    implement bubble sort

---
+++ sorting_and_searching/bubble_sort_algo_imple_2.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+
+int countSwap = 0;
+void bubbleSort(std::vector<int>&arr, int n) {
+
+	for(int i = 0; i < n - 1; i++) {
+		for(int j = 0; j < n - i - 1; j++) {
+			if(arr[j] > arr[j + 1]) {
+				std::swap(arr[j], arr[j + 1]);
+				countSwap++;
+			}
+		}
+	}
+}
+
+
+int main() {
+
+	int n;
+	std::cin >> n;
+	std::vector<int>arr;
+	for(int i = 0; i < n; i++) {
+		int x;
+		std::cin >> x;
+		arr.push_back(x);
+	}
+
+	bubbleSort(arr, n);
+	for(auto ele: arr) {
+		std::cout << ele << " ";
+	}
+	std::cout << '\n';
+	std::cout << countSwap << '\n';
+
+	return 0;
+}
