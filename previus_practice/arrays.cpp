2169f814388c94d9e925e8699b0040d4407940dd
bird <bird@bird.bird>
2022-01-15 04:38:11 -0500

    arrays

---
+++ previus_practice/arrays.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+
+
+int main() {
+
+	int arr[20];
+	int arr1[] = {1, 2, 3, 4, 5, 6};
+	int arr2[20] = {0};
+
+	// for(int i = 0 ; i < 20; i++) {
+	// 	std::cout << arr[i] << " ";
+	// }
+
+
+	for(int i = 0; i < 6; i++) {
+		std::cout << arr1[i]  <<" ";
+	}
+
+	for(int i = 0; i < 20; i++) {
+		std::cout << arr2[i] << " ";
+	}
+
+
+
+
+	return 0;
+}
