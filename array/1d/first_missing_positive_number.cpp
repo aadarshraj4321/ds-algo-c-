0be99e3ebdfc905ce3deb40a9f31814f72e8203c
aadarshraj4321 <aadarshraj4321@gmail.com>
2022-05-12 17:08:38 -0400

    array questions

---
+++ array/1d/first_missing_positive_number.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+int findMissingNumber(std::vector<int>arr) {
+	int num = arr.size();
+	for(int i = 0; i < num; i++) {
+		if(arr[i] <= 0 || arr[i] > num + 1) {
+			arr[i] = num + 2;
+		}
+	}
+	
+	for(auto no: arr) {
+		int negNumber = (no > 0) ? no - 1 : -no - 1;
+		if(negNumber >= 0 && negNumber < num) {
+			arr[negNumber] = -arr[negNumber];
+		}
+	}
+
+
+	for(int i = 0; i < num; i++) {
+		if(arr[i] > 0) {
+			return i + 1;
+		}
+	}
+	return num + 1; 
+}
+
+
+int main() {
+
+	std::vector<int>arr = {1, 1};
+	std::cout << findMissingNumber(arr) << '\n';
+
+	return 0;
+}
