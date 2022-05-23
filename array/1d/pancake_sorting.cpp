f20c363feb4cdc0969950b96fd28482c86e3ec66
aadarshraj4321 <aadarshraj4321@gmail.com>
2022-05-23 13:35:08 -0400

    array questions

--- array/1d/pancake_sorting.cpp
+++ array/1d/pancake_sorting.cpp
@@ -1,20 +1,9 @@
 #include<bits/stdc++.h>
 
-std::vector<int>flipThePancake(std::vector<int>arr) {
-
-	int n = arr.size();
-	std::vector<int>newArr;
-	while(n > 0) {
-		int idx = findIndex(arr, n);
-		
-	}
-}
-
 
 int main() {
 
-	std::vector<int>arr = {3,2,4,1};
-	auto panCake = flipThePancake(arr);
+
 
 	return 0;
 }
